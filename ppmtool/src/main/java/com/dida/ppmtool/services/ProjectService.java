2a65753cac7a869273a926e14631581b2006857c
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-01-03 17:51:14 -0800

    created custom exception for Project identifier

--- ppmtool/src/main/java/com/dida/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/dida/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.dida.ppmtool.services;
 
 import com.dida.ppmtool.domain.Project;
+import com.dida.ppmtool.exceptions.ProjectIdException;
 import com.dida.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,13 @@ public class ProjectService {
     //Logic
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
     }
 
 }
